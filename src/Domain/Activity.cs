4d8260e4cafca2778db001de834a77d856ee6851
baam <fer.barron.alvarez.16@gmail.com>
2026-09-09 22:53:48 -0600

    feat(P01) - add entity and EF boilerplate

---
+++ src/Domain/Activity.cs
@@ -0,0 +1,15 @@
+namespace Domain;
+// abstraction of real objects that will conform the system; classes that let us define/model our system
+public class Activity
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public required string Title { get; set; }
+    public DateTime Date { get; set; }
+    public required string Description { get; set; }
+    public required string Category { get; set; }
+    public bool IsCancelled { get; set; }
+    public required string City { get; set; }
+    public required string Venue { get; set; }
+    public required string Latitude { get; set; }
+    public required string Longitude { get; set; }
+}
---
+++ src/Persistence/AppDbContext.cs
@@ -0,0 +1,9 @@
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Activity> Activities{ get; set; }
+}
