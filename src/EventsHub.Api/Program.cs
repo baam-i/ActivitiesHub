959d0b0f0ba05d53da2d61f503a4e507e9e269e3
baam <fer.barron.alvarez.16@gmail.com>
2026-09-24 07:52:34 -0600

    feat(P02) - add edit event and AUTOMAPPER

--- src/EventsHub.Api/Program.cs
+++ src/EventsHub.Api/Program.cs
@@ -1,3 +1,4 @@
+using EventsHub.Application.Core;
 using EventsHub.Application.Events.Queries;
 using EventsHub.Persistence;
 using Microsoft.EntityFrameworkCore;
@@ -14,7 +15,7 @@ builder.Services.AddCors();
 builder.Services.AddMediatR(opt =>
     opt.RegisterServicesFromAssemblyContaining<GetEventList.Handler>()
 );
-
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
